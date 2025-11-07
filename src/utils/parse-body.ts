992c6ebe0a47084756f206dd552099c2f7c5ac15
Katsiaryna Mashko <katemashko@gmail.com>
2025-11-07 23:13:14 +0300

    feat: add parse-body file

---
+++ src/utils/parse-body.ts
@@ -0,0 +1,20 @@
+import * as http from "node:http";
+
+function parseBody(req: http.IncomingMessage) {
+  return new Promise((resolve, reject) => {
+    if (req.headers["content-type"] === "application/json") {
+      let body = "";
+
+      req.on("data", (chunk) => (body += chunk.toSting()));
+      req.on("end", () => {
+        try {
+          resolve(JSON.parse(body));
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+  });
+}
+
+export { parseBody };
